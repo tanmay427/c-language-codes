3ca792ed5de133a63c1233689c37c9679582ccb0
tanmay427 <thg67yp@gmail.com>
2026-08-10 10:03:06 +0530

    Add files via upload

---
+++ dividingtwonumber.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,division;
+    printf("enter two integers:");
+    scanf("%d %d",&num1,&num2);
+    division=num1/num2;
+    printf("division of two numbers =%d",division);
+    return 0;
+}
