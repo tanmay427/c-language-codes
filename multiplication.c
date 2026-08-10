c7ae6926145c96e632c2254683545e08bca48d19
tanmay427 <thg67yp@gmail.com>
2026-08-10 11:05:29 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,product;
+    printf("enter two integers:");
+    scanf("%d %d",&num1,&num2);
+    product=num1*num2;
+    printf("product of the two numbers=%d\n",product);
+
+return 0;
+}
